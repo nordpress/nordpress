3aa6ab5b46af863889c1e473f79f7f6f40ba5fef
Yish <mombuartworks@gmail.com>
2021-02-27 21:01:30 +0800

    dev tools

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,22 @@
 
 namespace App\Providers;
 
+use Barryvdh\Debugbar\Facade as DebugbarFacade;
+use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected array $providers = [
+        DebugbarServiceProvider::class,
+        IdeHelperServiceProvider::class
+    ];
+
+    protected array $aliases = [
+        'Debugbar' => DebugbarFacade::class,
+    ];
+
     /**
      * Register any application services.
      *
@@ -13,7 +25,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            foreach ($this->providers as $provider) {
+                $this->app->register($provider);
+
+                foreach ($this->aliases as $alias) {
+                    $this->app->alias($provider, $alias);
+                }
+            }
+        }
     }
 
     /**
